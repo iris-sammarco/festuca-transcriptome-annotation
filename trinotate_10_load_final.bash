65bd1fd3a68f87f7c0f104125941b3669a82df14
Iris Sammarco <58272801+iris-sammarco@users.noreply.github.com>
2026-04-23 17:09:16 +0200

    Update trinotate_10_load_final.bash

--- trinotate_10_load_final.bash
+++ trinotate_10_load_final.bash
@@ -17,14 +17,14 @@ trap 'echo "[ERROR] Line $LINENO: ${BASH_COMMAND} failed on $(date)" >&2; exit 1
 # Environment & Paths
 export TMPDIR=$SCRATCHDIR # This will force the application to place the temporary files into scratch directory instead of a /tmp directory
 module add mambaforge
-export CONDA_ENVS_PATH=/storage/pruhonice1-ibot/home/irissammarco/.conda/envs
+export CONDA_ENVS_PATH=/path/to/conda/envs
 mamba activate trinotate_env
 
-export TRINITY_HOME="/storage/pruhonice1-ibot/home/irissammarco/.conda/envs/trinotate_env/opt/trinity-2.8.5"
-export PATH="/storage/plzen1/home/irissammarco/Festuca_RNA_assembly/tmhmm-2.0c/bin:$PATH"
+export TRINITY_HOME="/path/to/conda/envs/trinotate_env/opt/trinity-2.8.5"
+export PATH="/path/to/project/tmhmm-2.0c/bin:$PATH"
 
 # Config
-OUTDIR="/storage/plzen1/home/irissammarco/Festuca_RNA_assembly/assembly/trinotate_output"
+OUTDIR="/path/to/project/assembly/trinotate_output"
 LOGDIR="${OUTDIR}/logs"
 PREFIX="Festuca_rubra"
 
