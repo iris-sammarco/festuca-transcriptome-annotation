7b1c11a9ea8a6ee1f615fd2ee2722a76e8e4fb8e
Iris Sammarco <58272801+iris-sammarco@users.noreply.github.com>
2026-04-23 17:42:28 +0200

    Update trinotate_07_blastp_wheat.bash

--- pipeline/trinotate_07_blastp_wheat.bash
+++ pipeline/trinotate_07_blastp_wheat.bash
@@ -34,6 +34,28 @@ MAX_TARGETS=5
 mkdir -p "${OUTDIR}" "${LOGDIR}"
 cd "${OUTDIR}"
 
+# Per script logs:
 exec 1> >(tee -a "${LOGDIR}/${PBS_JOBNAME}.log")
 exec 2> >(tee -a "${LOGDIR}/${PBS_JOBNAME}.err")
 echo "[INFO] Job ${PBS_JOBID} started in ${PWD} | Threads: ${THREADS}"
+
+## Sanity checks (fail-fast)
+[[ -s "${ASSEMBLY}" ]] || { echo "[FATAL] Missing assembly: ${ASSEMBLY}"; exit 1; }
+ln -sf "${ASSEMBLY}" Trinity.fasta
+
+## STEP 11: Wheat Curation based on their proteomes fasta files
+PEP_FINAL="Trinity.fasta.transdecoder.pep"
+if [[ ! -s "blastp.wheat.outfmt6" || ! -s ".wheat.done" ]]; then
+    echo "[INFO] Wheat proteome..."
+    rm -f *.wheat* .wheat.done
+    #wget -N --no-check-certificate \ "https://ftp.ensemblgenomes.org/pub/plants/release-62/fasta/triticum_aestivum/pep/Triticum_aestivum.IWGSC.pep.all.fa.gz"
+    #gunzip -f Triticum_aestivum.IWGSC.pep.all.fa.gz
+    #diamond makedb --in Triticum_aestivum.IWGSC.pep.all.fa -d wheat --threads ${THREADS}
+    #diamond blastp --db wheat.dmnd --query "${PEP_FINAL}" \
+    #    --out blastp.wheat.outfmt6 --evalue ${EVAL} --max-target-seqs ${MAX_TARGETS} --threads ${THREADS} --outfmt 6 \
+    makeblastdb -in Triticum_aestivum.IWGSC.pep.all.fa -dbtype prot -out wheat -blastdb_version 4
+    blastp -db wheat -query "${PEP_FINAL}" -out blastp.wheat.outfmt6 \
+        -evalue ${EVAL} -max_target_seqs ${MAX_TARGETS} -max_hsps 1 \
+        -num_threads ${THREADS} -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore" > "${LOGDIR}/wheat.log" 2>&1
+    touch .wheat.done
+fi
